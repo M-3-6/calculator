8829775b8f6faa5aa475cf5ccc5baf8ca573d08b
M-3-6 <63166735+M-3-6@users.noreply.github.com>
2020-08-15 01:21:49 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -26,6 +26,6 @@ app.post("/",function(req,res){
   }
   res.send("the result is: "+result);
 });
-app.listen(process.env.PORT||3000,function(){
+app.listen(process.env.PORT || 3000,function(){
   console.log("server has started");
 });
